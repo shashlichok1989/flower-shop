8d89dc2c6c0209e766d1d4f04857e25022aee944
shashlichok1989 <55533843+shashlichok1989@users.noreply.github.com>
2021-02-05 10:29:45 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+  const flowerSlider = new Swiper(".flowers-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //settings for media version
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 6
+    }
+    
+  }
+});
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+  $("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+      opts : {
+        caption : 'First caption',
+        thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+      }
+    },
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+    ,
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+});
